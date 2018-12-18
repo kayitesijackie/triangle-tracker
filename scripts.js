d9e9199e18e7ee2d1615395fc68b28a254de6314
Kayitesi Jacqueline <kayitesijacqueline@gmail.com>
2018-12-18 18:29:36 +0200

    add array

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 function getResult(){
+	var triangle = [side1, side2, side3];
 	var side1 = parseFloat(document.getElementById('side1').value);
 	var side2 = parseFloat(document.getElementById('side2').value);
 	var side3 = parseFloat(document.getElementById('side3').value);
